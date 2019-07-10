60f09342bb908b3bb599c13fe7847aac7ad49d9d
Joanna Erd <joanna.erd@elpassion.pl>
2019-07-10 15:51:52 +0200

    Initialize CRA app with linting

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  'parser': '@typescript-eslint/parser',
+  'extends': [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+  ],
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'babel',
+    'import',
+    'react',
+    'react-hooks',
+    'jest',
+  ],
+  'rules': {
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'babel/no-invalid-this': 'error',
+    'babel/semi': 'warn',
+    'babel/no-unused-expressions': 'warn',
+    eqeqeq: ['error', 'always'],
+    curly: 'error',
+    'no-var': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'no-console': ['error', { allow: ['error', 'warn'] }],
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        'newlines-between': 'always'
+      }
+    ],
+    'prefer-const': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  }
+};
