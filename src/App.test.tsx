ed8fa72f6b3837ba6d0c613ac926bf496a49ab6e
Joanna Erd <joanna.erd@elpassion.pl>
2019-07-11 14:03:46 +0200

    Add cypress and react testing libraries

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import {
+  render,
+} from '@testing-library/react'
 
 import { App } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders "Learn React" text', () => {
+  const { getByText } = render(<App />);
+  getByText('Learn React');
 });
