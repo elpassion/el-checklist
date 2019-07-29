f1c33bc7d765cfd682a2dc9f88434aab45116ce2
Joanna Erd <joanna.erd@elpassion.pl>
2019-07-29 13:27:36 +0200

    Add sections length check

--- src/components/Checklist/Checklist.tsx
+++ src/components/Checklist/Checklist.tsx
@@ -15,17 +15,24 @@ export const Checklist: React.FC<TProps> = ({ checklist }: TProps) => {
     <>
       <h1>{checklist.name}</h1>
 
-      {checklist.sections.map(section => (
-        <section key={section.name}>
-          <h2>{section.name}</h2>
+      {checklist.sections &&
+        checklist.sections.map(section => (
+          <section key={section.name}>
+            <h2>{section.name}</h2>
 
-          <ul>
-            {section.items.map(item => (
-              <ChecklistItem {...item} key={item.slug} Tag="li" isChecked={isFulfilled(item.id)} onChange={onChange} />
-            ))}
-          </ul>
-        </section>
-      ))}
+            <ul>
+              {section.items.map(item => (
+                <ChecklistItem
+                  {...item}
+                  key={item.slug}
+                  Tag="li"
+                  isChecked={isFulfilled(item.id)}
+                  onChange={onChange}
+                />
+              ))}
+            </ul>
+          </section>
+        ))}
     </>
   );
 };
